64be377f780f46f25d54ac1a43b8312ef77193ed
eneasvirgilio <55248002+eneasvirgilio@users.noreply.github.com>
2021-01-18 13:15:20 -0300

    utilizacion de API

---
+++ src/Prueba2.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+
+
+public class Prueba2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+           String nombre;
+           Scanner miomio;
+           Locale miomio2;
+	}
+
+}
